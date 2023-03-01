23bb536d4884b76b82c43cdd62103f22fd60ee65
Julia Machina <machinajg@gmail.com>
2023-03-01 18:14:26 +0300

    Recursion numbers from M to N project added

---
+++ recursion_M_N/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+//  в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Message(string text)
+{
+    Console.Write(text);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+int SegmentRecursion(int m, int n)
+{
+    if (m == n) return m;
+    else return m + SegmentRecursion((m + 1), n);
+}
+int m = Message("введи значение M: ");
+int n = Message("введи значение N: ");
+Console.WriteLine(SegmentRecursion(m, n));
